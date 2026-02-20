074484b25b56e91b4de89adda96242a63d6e662f
Vinayak Singh <volcof@localhost.localdomain>
2026-02-21 02:08:13 +0530

    working model!

--- model.c
+++ model.c
@@ -1,17 +1,28 @@
-#include <signal.h>
-#include <setjmp.h>
-#include <stdlib.h>
+#include <complex.h>
 #include <stdio.h>
-#define return 0 r0
+#include <math.h>
 
-int IQ[5] = { 21 , 90 , 30 , 40 , 60 };
-int HRS[
+int USR[20] = {21,7,5,15,30,8,12,25,18,6,22,9,14,28,11,19,4,16,23,10};
+int IRR[20] = {0,1,1,1,0,1,1,0,1,1,0,1,1,0,1,0,1,1,0,1};
+int IST[20] = {0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,0,1,1,0,1};
+double Ps[20];
+int n = sizeof(USR)/sizeof(USR[0]);
 
-int goesWrong(int signal){
-    puts("Something went wrong, exiting!");
-    exit(EXIT_FAILURE);
+double sigmoid(double z){
+    double p = (1)/(1+exp(-z));
+    return p;
 }
 
 int main(void){
-
+    double w0=0.1;
+    double w1=0.1;
+    double w2=0.1;
+    double z;
+    for(int pos  = 0; pos < n ; pos++){
+        z=w0+(w1)*(USR[pos])+(w2)*(IRR[pos]);
+        Ps[pos]=sigmoid(z);
+        printf("z = %f\tsigmoid = %f\n",z,Ps[pos]);
+    }
+    return 0;
+}
 
