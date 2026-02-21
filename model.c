c8556e8a91454083f322130ea732c441046bab33
Vinayak Singh <volcof@localhost.localdomain>
2026-02-21 10:08:13 +0530

    First epoch completed.

--- model.c
+++ model.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 
+const double alpha = 0.01;
 int USR[20] = {21,7,5,15,30,8,12,25,18,6,22,9,14,28,11,19,4,16,23,10};
 int IRR[20] = {0,1,1,1,0,1,1,0,1,1,0,1,1,0,1,0,1,1,0,1};
 int IST[20] = {0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,0,1,1,0,1};
 double Ps[20];
 int n = sizeof(USR)/sizeof(USR[0]);
+double s0,s1,s2;
 
 double sigmoid(double z){
     double p = (1)/(1+exp(-z));
@@ -22,7 +24,14 @@ int main(void){
         z=w0+(w1)*(USR[pos])+(w2)*(IRR[pos]);
         Ps[pos]=sigmoid(z);
         printf("z = %f\tsigmoid = %f\n",z,Ps[pos]);
+        s0 += (Ps[pos]-IST[pos]);
+        s1 += (Ps[pos]-IST[pos])*(USR[pos]);
+        s2 += (Ps[pos]-IST[pos])*(IRR[pos]);
     }
+    w0 = w0 - alpha*s0 ;
+    w1 = w1 - alpha*s1 ;
+    w2 = w2 - alpha*s2 ;
+    printf("Adjusted:\n\tw0\t:\t%f\n\tw1\t:\t%f\n\tw2\t:\t%f\n",w0,w1,w2);
     return 0;
 }
 
